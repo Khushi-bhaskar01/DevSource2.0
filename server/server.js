cc6bc26c0d4eedf0d2ac61dbc110f4aadc27ce31
KHUSHI BHASKAR <khushibhaskar.2006@gmail.com>
2026-01-18 00:56:58 +0530

    Update CORS origin to include Vercel app

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173","https://dev-source2-0.vercel.app/"
     credentials: true,
   })
 );
