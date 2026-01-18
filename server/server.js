4bc1cbf06f00a66462d51e9e9ff2f841a4837225
KHUSHI BHASKAR <khushibhaskar.2006@gmail.com>
2026-01-18 09:28:50 +0530

    Reorder import statements and clean up code

--- server/server.js
+++ server/server.js
@@ -3,21 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+
 import authRouter from "./routes/authRouter.js";
-import errorMiddleware from "./middleware/error.js";
 import userRouter from "./routes/userRouter.js";
 import superAdminRoutes from "./routes/SuperAdminRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import submissionRoutes from "./routes/submissionRoutes.js";
 import leaderboardRoute from "./routes/leaderboardRoute.js";
 import profileRouter from "./routes/profileRouter.js";
+import errorMiddleware from "./middleware/error.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-
 app.set("trust proxy", 1);
-
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
@@ -33,9 +32,6 @@ app.use(
     credentials: true,
   })
 );
-
-
-app.options("*", cors());
 
 app.get("/", (req, res) => res.send("Api Working!"));
 
